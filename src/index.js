a14032f3dedd5a6c0301063ed49629d3176859cd
IsuruGodakanda <isurugreatcode@gmail.com>
2020-06-11 13:34:47 +0530

    Integrate ServiceWorker

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Make sure sw are supported
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('./sw_cached_pages.js')
+      .then((reg) => console.log('Service Worker: Registered (Pages)'))
+      .catch((err) => console.log(`Service Worker: Error: ${err}`));
+  });
+}
